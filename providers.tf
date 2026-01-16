7f0bfd2f7579525c863e2a2ee060814933f83848
Daniel Morris <daniel@honestempire.com>
2026-01-16 06:44:09 +0000

    feat: Add create variable and configure resources
    This adds a create variable and updates the count property on all
    resources and data sources to consider the variable, updates the outputs
    to avoid errors if nothing was created.

--- providers.tf
+++ providers.tf
@@ -1,6 +1,3 @@
-// SPDX-FileCopyrightText: 2023 Daniel Morris <daniel@honestempire.com>
-// SPDX-License-Identifier: MIT
-
 provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
--- variables.tf
+++ variables.tf
@@ -3,65 +3,71 @@
 
 variable "bucket_name" {
   default     = ""
-  description = "The name of the S3 bucket that will store the website."
+  description = "Name of the S3 bucket that will store the website."
   type        = string
 }
 
 variable "bucket_name_logs" {
   default     = null
-  description = "The name of the S3 bucket that will store logs."
+  description = "Name of the S3 bucket that will store logs."
   type        = string
+}
+
+variable "create" {
+  default     = true
+  description = "Enable/disable the creation of all resources."
+  type        = bool
 }
 
 variable "create_certificate" {
   default     = true
-  description = "A flag to enable/disable the creation of an ACM certificate."
+  description = "Enable/disable the creation of an ACM certificate."
   type        = bool
 }
 
 variable "create_cloudfront_distribution" {
   default     = true
-  description = "A flag to enable/disable the creation of a CloudFront distribution."
+  description = "Enable/disable the creation of a CloudFront distribution."
   type        = bool
 }
 
 variable "create_log_bucket" {
   default     = true
-  description = "A flag to enable/disable the creation of a log bucket."
+  description = "Enable/disable the creation of a log bucket."
   type        = bool
 }
 
 variable "domain_name" {
-  description = "The domain name of the website."
+  description = "Domain name of the website."
   type        = string
 }
 
 variable "enable_logging" {
   default     = true
-  description = "A flag to enable/disable logging on the S3 bucket."
+  description = "Enable/disable logging on the S3 bucket."
   type        = bool
 }
 
 variable "enable_versioning" {
   default     = true
-  description = "A flag to enable/disable versioning on the S3 bucket."
+  description = "Enable/disable versioning on the S3 bucket."
   type        = bool
 }
 
 variable "error_document" {
   default     = "error.html"
-  description = "The document returned when a 4xx error occurs."
+  description = "Document returned when a 4xx error occurs."
   type        = string
 }
 
 variable "index_document" {
   default     = "index.html"
-  description = "The document returned for directory requests."
+  description = "Document returned for directory requests."
   type        = string
 }
 
 variable "tags" {
   default     = {}
-  description = "A map of tags to apply to all applicable resources."
+  description = "Tags to apply to all applicable resources."
   type        = map(string)
 }
