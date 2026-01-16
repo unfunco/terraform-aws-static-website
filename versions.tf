25a601406fccddbc8ecb5d09c3caa4f5448df37b
Daniel Morris <daniel@honestempire.com>
2026-01-16 07:24:43 +0000

    chore: Improve the description in the README

--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@
 // SPDX-License-Identifier: MIT
 
 terraform {
+  required_version = "~> 1.14"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
-
-  required_version = "~> 1.14"
 }
